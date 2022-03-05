b5896ff5ece4e6d105c4929f43991238944fab9f
sky9023 <25349023.qq@gmail.com>
2022-03-05 14:12:07 +0800

    [UPDATE] read input

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
+#include <vector>
+#include <tuple>
 
 
-class TransactionDB;
+using TransactionDB = std::vector<std::vector<int>>;
 
 /// <h1>Input</h1>
 
 TransactionDB read_transaction_file(const std::string &in_fname) {
+    TransactionDB transactions(1);
+    std::ifstream fin(in_fname);
+    fin >> std::noskipws;
+    int data;
+    char delim;
+    while (fin >> data >> delim) {
+        transactions.back().push_back(data);
 
+        if (delim == '\n') {
+            transactions.emplace_back();
+        }
+    }
+    if (transactions.back().empty()) {
+        transactions.pop_back();
+    }
+
+    return transactions;
 }
 
 /// <h1>FP Tree</h1>
@@ -34,7 +53,7 @@ void FPTree::_build_header_table(const std::string &in_fname, double min_supp) {
 
 
 int main(int argc, char **argv) {
-    if (argc != 3) {
+    if (argc != 4) {
         std::cerr << "invalid argument!" << std::endl;
         std::exit(1);
     }
@@ -43,7 +62,14 @@ int main(int argc, char **argv) {
     std::cout << min_support << std::endl;
     std::string in_filename = std::string(argv[2]);
     std::string out_filename = std::string(argv[3]);
+    auto transactions = read_transaction_file(in_filename);
 
+    for (const auto &transaction: transactions) {
+        for (const auto &item: transaction) {
+            std::cout << item << " ";
+        }
+        std::cout << std::endl;
+    }
 
     return 0;
 }
