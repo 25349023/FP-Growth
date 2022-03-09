b8e20a8055d2ceb58dca279a5bef6b66a401aa2f
sky9023 <25349023.qq@gmail.com>
2022-03-09 22:46:27 +0800

    [UPDATE] construct conditional FP tree

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include <cmath>
 #include <functional>
 #include <iomanip>
+#include <deque>
 
 
 using Item = int;
@@ -63,16 +64,16 @@ TransactionDB read_transaction_file(const std::string& in_fname) {
 struct TreeNode {
     using NodeMapping = std::unordered_map<const TreeNode*, TreeNode*>;
 
-    Item data;
+    Item item;
     int count{1};
     TreeNode* left_child{};
     TreeNode* right_sibling{};
     TreeNode* cross_link{};
     TreeNode* parent{};
 
-    explicit TreeNode(Item x, TreeNode* p = nullptr) : data(x), parent(p) {}
+    explicit TreeNode(Item x, TreeNode* p = nullptr) : item(x), parent(p) {}
 
-    TreeNode(const TreeNode& other) : data(other.data), count(other.count) {}
+    TreeNode(const TreeNode& other) : item(other.item), count(other.count) {}
 
     // Add new node(x) to child, or increment the count if the node(x) already exists.
     // Then return the affected node and whether the insertion is successful or not
@@ -84,7 +85,7 @@ struct TreeNode {
 
         TreeNode* prev = nullptr;
         for (auto child = left_child; child != nullptr; prev = child, child = child->right_sibling) {
-            if (child->data == x) {
+            if (child->item == x) {
                 child->count++;
                 return std::make_pair(child, false);
             }
@@ -103,10 +104,86 @@ struct TreeNode {
         return std::make_pair(std::move(ht), new_root);
     }
 
-    // delete the child tree
-    void prune() {
+    // delete the subtree
+    void prune_subtree() {
         delete left_child;
         left_child = nullptr;
+    }
+
+    // Return true if `x` not in any subtree and thus can be removed.
+    // Does NOT handle cross-links, so after this action, the cross-links of
+    // nodes whose item is not `x` will become invalid
+    bool remove_subtrees_without(Item x) {
+        if (!left_child) {
+            return item != x;
+        }
+
+        bool all_without_x = true;
+        TreeNode* prev{};
+        for (auto child = left_child; child != nullptr;) {
+            bool without_x = child->remove_subtrees_without(x);
+            all_without_x = all_without_x && without_x;
+
+            if (without_x) {
+                auto temp = child->right_sibling;
+                if (prev) {
+                    prev->right_sibling = child->right_sibling;
+                } else {
+                    // child is the leftmost child
+                    left_child = child->right_sibling;
+                }
+                child->right_sibling = nullptr;
+                delete child;
+                child = temp;
+            } else {
+                prev = child;
+                child = child->right_sibling;
+            }
+        }
+
+        return all_without_x;
+    }
+
+    void append_to_child(TreeNode* target) {
+        if (!left_child) {
+            left_child = target;
+            return;
+        }
+
+        auto curr = left_child;
+        for (; curr->right_sibling; curr = curr->right_sibling) {}
+        curr->right_sibling = target;
+
+        for (auto t = target; t; t = t->right_sibling) {
+            t->parent = this;
+        }
+    }
+
+    void merge_children() {
+        if (!left_child) {
+            return;
+        }
+
+        std::map<Item, TreeNode*> exists;
+        TreeNode* prev{};
+        for (auto curr = left_child; curr;) {
+            if (exists.find(curr->item) == exists.end()) {
+                exists[curr->item] = curr;
+                prev = curr;
+                curr = curr->right_sibling;
+            } else {
+                // merge counts
+                auto major_node = exists[curr->item];
+                major_node->count += curr->count;
+                major_node->append_to_child(curr->left_child);
+                curr->left_child = nullptr;
+
+                // remove curr from tree
+                prev->right_sibling = curr->right_sibling;
+                delete curr;
+                curr = prev->right_sibling;
+            }
+        }
     }
 
     ~TreeNode() {
@@ -153,7 +230,7 @@ private:
 class FPTree {
 public:
     explicit FPTree(TransactionDB&& transactions, double ms) :
-        transactions(std::move(transactions)), min_support(ms) {
+        transactions(std::move(transactions)), min_support_count(ms * transactions.size()) {
         build();
     }
 
@@ -163,6 +240,13 @@ public:
         exclude_non_frequent_items();
         sort_transaction_items();
         construct_fp_tree();
+    }
+
+    // mining will make some unrecoverable changes to the tree, so be sure that the tree is copied
+    void mine(Item x) {
+        prune_by(x);
+        refresh_counts(x);
+        construct_conditional_fp_tree(x);
     }
 
     void print_transaction_db() {
@@ -184,7 +268,7 @@ public:
     void traverse_cross_links() {
         for (const auto& links: header_table) {
             for (TreeNode* curr = links.second.first; curr != nullptr; curr = curr->cross_link) {
-                std::cout << curr->data << " : " << curr->count << ", ";
+                std::cout << curr->item << " : " << curr->count << ", ";
             }
             std::cout << std::endl;
         }
@@ -194,12 +278,11 @@ public:
     // there is no need to copy transactions and frequent_items
     // since they will not participate in the mining stage
     FPTree(const FPTree& oth)
-        : min_support(oth.min_support), item_counter(oth.item_counter) {
+        : min_support_count(oth.min_support_count), item_counter(oth.item_counter) {
         std::tie(header_table, root) = oth.root->copy(oth.header_table);
     }
 
     ~FPTree() {
-        // TODO: delete the tree
         delete root;
     }
 
@@ -214,6 +297,15 @@ private:
 
     void construct_fp_tree();
 
+    void prune_by(Item x);
+
+    void refresh_counts(Item x);
+
+    void construct_conditional_fp_tree(Item x);
+
+    void mine_conditional_fp_tree(Item x);
+
+
     bool is_frequent(const Item item) {
         return frequent_items.find(item) != frequent_items.end();
     }
@@ -227,15 +319,15 @@ private:
     }
 
     TransactionDB transactions;
-    double min_support;
+    double min_support_count;
     std::map<Item, int> item_counter{};
     std::unordered_set<Item> frequent_items{};
     HeaderTable header_table{};
     TreeNode* root{};
+    bool remove_one_child(TreeNode* node);
 };
 
 void FPTree::find_frequent_items() {
-    double min_support_count = min_support * transactions.size();
     for (const auto& transaction: transactions) {
         for (const auto& item: transaction) {
             item_counter[item]++;
@@ -304,6 +396,76 @@ void FPTree::construct_fp_tree() {
             curr = node;
         }
     }
+}
+
+void FPTree::prune_by(Item x) {
+    root->remove_subtrees_without(x);
+}
+
+void FPTree::refresh_counts(Item x) {
+    auto links = header_table[get_key(x)];
+
+    // refresh item counts in a bottom-up manner
+    for (auto curr = links.first; curr != nullptr; curr = curr->cross_link) {
+        for (auto p = curr->parent; p != nullptr; p = p->parent) {
+            p->count = 0;
+        }
+    }
+    for (auto curr = links.first; curr != nullptr; curr = curr->cross_link) {
+        for (auto p = curr->parent; p != nullptr; p = p->parent) {
+            p->count += curr->count;
+        }
+    }
+}
+
+void FPTree::construct_conditional_fp_tree(Item x) {
+    std::deque<TreeNode*> nodes_to_be_check;
+    nodes_to_be_check.emplace_back(root);
+
+    while (!nodes_to_be_check.empty()) {
+        auto node = nodes_to_be_check.front();
+        do {
+            node->merge_children();
+        } while (remove_one_child(node));
+
+        for (auto child = node->left_child; child; child = child->right_sibling) {
+            nodes_to_be_check.push_back(child);
+        }
+        nodes_to_be_check.pop_front();
+    }
+}
+
+bool FPTree::remove_one_child(TreeNode* node) {
+    if (!node->left_child) {
+        return false;
+    }
+
+    // pair<prev, curr>
+    std::pair<TreeNode*, TreeNode*> candidate;
+
+    TreeNode* prev{};
+    for (auto curr = node->left_child; curr; prev = curr, curr = curr->right_sibling) {
+        if (curr->count < min_support_count) {
+            if (!candidate.second ||
+                frequent_than(curr->item, candidate.second->item)) {
+                candidate = std::make_pair(prev, curr);
+            }
+        }
+    }
+    if (candidate.second) {
+        candidate.first->right_sibling = candidate.second->right_sibling;
+        node->append_to_child(candidate.second->left_child);
+        candidate.second->left_child = nullptr;
+        candidate.second->right_sibling = nullptr;
+        delete candidate.second;
+        return true;
+    }
+
+    return false;
+}
+
+void FPTree::mine_conditional_fp_tree(Item x) {
+
 }
 
 
